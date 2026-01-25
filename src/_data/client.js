2ddd821974389116453748953ce4f5ea338ab244
Taylor <Taylorkellycodes@gmail.com>
2026-01-25 10:54:55 -0500

    first round with SEO, need pics

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,23 +1,21 @@
 module.exports = {
-    name: "Code Stitch Web Designs",
-    email: "help@codestitch.app",
-    phoneForTel: "555-779-4407",
-    phoneFormatted: "(555) 779-4407",
+    name: "M&M Crawlspace Renovations",
+    email: "mmcrawlspace@gmail.com",
+    phoneForTel: "910-635-4943",
+    phoneFormatted: "(910) 635-4943",
     address: {
-        lineOne: "First Address Line",
-        lineTwo: "Second Address Line",
-        city: "Denver",
-        state: "CO",
-        zip: "80206",
+        city: "Cary",
+        state: "NC",
+        zip: "27511",
         country: "US",
-        mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
+        mapLink: "https://maps.app.goo.gl/gtSBT6UEqSdoYymy6",
     },
     socials: {
         facebook: "https://www.facebook.com/",
         instagram: "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.example.com",
+    domain: "https://www.mmcrawlspacerenovations.com",
     // Passing the isProduction variable for use in HTML templates
     isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
